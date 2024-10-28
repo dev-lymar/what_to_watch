2e26971a3826318a0b2612edc38f1ab987c233fa
Lymar Volodymyr <shaxtar11@gmail.com>
2024-10-28 23:39:32 +0100

    Added templates. Edited index_view for display index.html

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from flask import Flask
+from datetime import datetime
+from random import randrange
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now())
+
+
 @app.route('/')
-def index():
-    print(app.config)
-    return 'Hello, World!'
+def index_view():
+    quantity = Opinion.query.count()
+
+    if not quantity:
+        return 'No opinions in database'
+
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return render_template('index.html', opinion=opinion)
 
 
 if __name__ == '__main__':
